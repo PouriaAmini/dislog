767c52811f61bd7cd9398c9fce860421fe0caef1
Pouria Amini <aminipooria1@gmail.com>
2023-02-14 16:37:22 -0500

    Implement the server

--- internal/log/log.go
+++ internal/log/log.go
@@ -1,7 +1,6 @@
 package log
 
 import (
-	"fmt"
 	api "github.com/pouriaamini/proglog/api/v1"
 	"io"
 	"io/ioutil"
@@ -91,7 +90,7 @@ func (l *Log) Read(off uint64) (*api.Record, error) {
 		}
 	}
 	if s == nil || s.nextOffset <= off {
-		return nil, fmt.Errorf("offset out of range: %d", off)
+		return nil, api.ErrOffsetOutOfRange{Offset: off}
 	}
 	return s.Read(off)
 }
