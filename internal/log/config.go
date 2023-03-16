a3d13ba30540731c1f217660849cf58fc18857f8
Pouria Amini <aminipooria1@gmail.com>
2023-03-16 13:52:08 -0400

    Add documentation for config implementation

--- internal/log/config.go
+++ internal/log/config.go
@@ -1,9 +1,16 @@
 package log
 
+// Config defines the configuration for the log
 type Config struct {
+	// Segment contains the configuration options for the log segments
 	Segment struct {
+		// MaxStoreBytes specifies the maximum size of a segment file for
+		// storing log entries
 		MaxStoreBytes uint64
+		// MaxIndexBytes specifies the maximum size of a segment file for
+		// storing index entries
 		MaxIndexBytes uint64
+		// InitialOffset specifies the initial offset value for the log
 		InitialOffset uint64
 	}
 }
