fbe3cd143dde8d59de03dbb32e911445f0bc5921
Pouria Amini <aminipooria1@gmail.com>
2023-03-17 21:46:43 -0400

    Add type wrapping Serf to provide discovery and cluster membership

---
+++ internal/discovery/membership.go
@@ -0,0 +1,27 @@
+package discovery
+
+import (
+	"go.uber.org/zap"
+
+	"github.com/hashicorp/serf/serf"
+)
+
+type Membership struct {
+	Config
+	handler Handler
+	serf    *serf.Serf
+	events  chan serf.Event
+	logger  *zap.Logger
+}
+
+func New(handler Handler, config Config) (*Membership, error) {
+	c := &Membership{
+		Config:  config,
+		handler: handler,
+		logger:  zap.L().Named("membership"),
+	}
+	if err := c.setupSerf(); err != nil {
+		return nil, err
+	}
+	return c, nil
+}
