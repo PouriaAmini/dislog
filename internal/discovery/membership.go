b1946bcf5f7bdb504606934b5499ee34ce740d34
Pouria Amini <aminipooria1@gmail.com>
2023-03-19 11:10:15 -0400

    Handle the error for non leader instances on Debug mode

--- internal/discovery/membership.go
+++ internal/discovery/membership.go
@@ -1,6 +1,7 @@
 package discovery
 
 import (
+	"github.com/hashicorp/raft"
 	"go.uber.org/zap"
 	"net"
 
@@ -117,7 +118,11 @@ func (m *Membership) Leave() error {
 }
 
 func (m *Membership) logError(err error, msg string, member serf.Member) {
-	m.logger.Error(
+	log := m.logger.Error
+	if err == raft.ErrNotLeader {
+		log = m.logger.Debug
+	}
+	log(
 		msg,
 		zap.Error(err),
 		zap.String("name", member.Name),
